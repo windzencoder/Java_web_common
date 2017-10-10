2853b9a4ecb2d293e2667bd66f0c547959ffddf5
wangzhen <869158130@qq.com>
2017-10-10 13:57:09 +0800

    shiro helloworld 例子

---
+++ shiro/src/main/java/com/wz/shiro/HelloWorld.java
@@ -0,0 +1,39 @@
+package com.wz.shiro;
+
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//读取配置文件
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		//获取SecurityManager实例
+		SecurityManager securityManager = factory.getInstance();
+		//绑定给SecurityUtils
+		SecurityUtils.setSecurityManager(securityManager);
+		//得到当前执行的用户
+		Subject currentUser = SecurityUtils.getSubject();
+		//创建token令牌
+		UsernamePasswordToken token = new UsernamePasswordToken("wz", "123456");
+		try {
+			//身份认证
+			currentUser.login(token);
+			System.out.println("身份认证成功");
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			System.out.println("身份认证失败");
+		}
+		//退出
+		currentUser.logout();
+		System.out.println("logout");
+
+	}
+
+}

4f5961676adf0bae7ff55840963947eaa4b6492f
wangzhen <869158130@qq.com>
2017-11-21 10:59:25 +0800

    shiro

--- shiro/tudou/04/shiro-web/src/main/java/com/wz/shiroweb/dao/ResourceMapper.java
+++ shiro/tudou/04/shiro-web/src/main/java/com/wz/shiroweb/dao/ResourceMapper.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface ResourceMapper {
+	
     long countByExample(ResourceExample example);
 
     int deleteByExample(ResourceExample example);
